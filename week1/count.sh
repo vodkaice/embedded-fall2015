a9cad974e58cacd9878df9d3c7141d354dac8774
vodkaice <brandedwolf@gmal.com>
2015-10-04 12:45:55 +0800

    execute one more program and plot the result
    execute avx_pi and baseline
    and output the result to the file
    then execute gnuplot to plot the result

--- week1/count.sh
+++ week1/count.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 size="1 2 4 8 16 32 64 128 256 512 1024"
-rm -f result.txt
+rm -f result_a.txt result_b.txt
+rm -f result_time.png result_error.png
 for N in $size
 do 
 	#c=$(( $N * 2 ))
 	#echo "N = $N"
-	./avx_pi $N >> result.txt
+	./avx_pi $N >> result_a.txt
 done	
 
+for N in $size
+do 
+	./baseline $N >> result_b.txt
+done	
+gnuplot -e "load './plot.txt'"
