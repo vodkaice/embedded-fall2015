d8fbefae9b4dc638216427c5745f288ef14de4d0
vodkaice <brandedwolf@gmal.com>
2015-10-04 12:43:25 +0800

    modify the fomat of output for gnuplot
    execute the program with argument
    and output the time and error

--- week1/baseline.c
+++ week1/baseline.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+#include <time.h>
+#define SEC 1000000000.0
+const double pi = 3.1415926535;
 double compute_pi(size_t dt){
 	size_t i;
 	double pi =0.0;
@@ -11,6 +13,12 @@ double compute_pi(size_t dt){
 	return pi*4.0;
 }
 
-int main(){
-	printf("%.10lf\n",compute_pi(128*1000000));
+int main(int argc, char* argv[]){
+	int N = atoi(argv[1]);
+	struct timespec start,end;
+	clock_gettime(CLOCK_MONOTONIC_RAW,&start);	//start time
+	double my_pi = compute_pi(N*1024*1024);
+	clock_gettime(CLOCK_MONOTONIC_RAW,&end);	//end time
+	printf("%lf %.10lf\n",(double)((end.tv_sec - start.tv_sec)+(end.tv_nsec - start.tv_nsec)/SEC),pi-my_pi);
+	return 0;
 }
